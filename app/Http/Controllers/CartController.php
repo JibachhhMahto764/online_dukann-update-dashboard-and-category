30e18d0448cdaa1697ec8346397c6c63ec7aa30b
Jibachh Singh <84733181+JibachhhMahto764@users.noreply.github.com>
2024-10-20 16:13:12 +0545

    cart not working

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -2,15 +2,42 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Product;
+use Gloudemans\Shoppingcart\Facades\Cart;
 use Illuminate\Http\Request;
 
 class CartController extends Controller
 {
-   public function addToCart(){
+   public function addToCart(Request $request){
+      $product = Product::with('product_images')->find('$request->id');
+
+      if ($product == null){
+         return response()->json([
+           'status' => false,
+           'message' => 'product not found!!!'
+         ]);
+      }
+      if (Cart::count() > 0){
+
+         echo "product already in the cart";
+
+
+      }else{
+         echo "product added  in the cart";
+         // when the cart is empty then 
+         Cart::add( $product->id, $product->title, 1, $product-> price,['productImage' => (!empty($product->product_images)) ? $product->product_images->first() : '']);
+
+         $status = true;
+         $message = $product->title.'added in cart';
+      }
+      return response()->json([
+         'status' => $status,
+         'message' => $message
+       ]);
 
    }
    public function Cart(){
-    
-    return view('front.cart');
+    dd(Cart::content());
+   // return view('front.cart');
    }
 }
--- resources/views/front/layouts/app.blade.php
+++ resources/views/front/layouts/app.blade.php
@@ -72,7 +72,6 @@
 		</div>
 	</div>
 </div>
-
 <header class="bg-dark">
 	<div class="container">
 		<nav class="navbar navbar-expand-xl" id="navbar">
@@ -81,32 +80,31 @@
 				<span class="h2 text-uppercase text-white px-2">SHOP</span>
 			</a>
 			<button class="navbar-toggler menu-btn" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      			<!-- <span class="navbar-toggler-icon icon-menu"></span> -->
 				  <i class="navbar-toggler-icon fas fa-bars"></i>
-    		</button>
+			</button>
     		<div class="collapse navbar-collapse" id="navbarSupportedContent">
       			<ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        			<!-- <li class="nav-item">
-          				<a class="nav-link active" aria-current="page" href="index.php" title="Products">Home</a>
-        			</li> -->
-                        @if (getCategories()->isNotEmpty())
+                    @if (getCategories()->isNotEmpty())
                         @foreach (getCategories() as $category)
-                        
-                        <li class="nav-item dropdown">
-						<button class="btn btn-dark dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
-							{{ $category-> name }}
-						</button>
-                        @if ($category->sub_category->isNotEmpty())
-                        <ul class="dropdown-menu dropdown-menu-dark"></ul>
-                        @foreach ($category->sub_category as $subCategory )
-                        <li><a class="dropdown-item nav-link" href="{{route('front.shop',[$category->slug,$subCategory->slug])}}">{{ $subCategory->name}}</a></li>
+                            <li class="nav-item dropdown">
+                                <button class="btn btn-dark dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
+                                    {{ $category->name }}
+                                </button>
+                                @if ($category->sub_category->isNotEmpty())
+                                    <ul class="dropdown-menu dropdown-menu-dark">
+                                        @foreach ($category->sub_category as $subCategory)
+                                            <li>
+                                                <a class="dropdown-item nav-link" href="{{ route('front.shop', [$category->slug, $subCategory->slug]) }}">
+                                                    {{ $subCategory->name }}
+                                                </a>
+                                            </li>
+                                        @endforeach
+                                    </ul>
+                                @endif
+                            </li>
                         @endforeach
-                        @endif
-					</li>
-
-                        @endforeach
-                        @endif
-      			</ul>      			
+                    @endif
+      			</ul>
       		</div>   
 			<div class="right-nav py-0">
 				<a href="cart.php" class="ml-3 d-flex pt-2">
